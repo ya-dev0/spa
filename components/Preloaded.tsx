12b4403068c89596ac5e20a690248f3d93acf28b
ya-dev0 <demoyuam1@proton.me>
2024-10-24 15:05:29 +0300

    Create Preloaded.tsx

---
+++ components/Preloaded.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Preloader() {
+  return (
+    <div className="preloader">
+      <div className="spinner">
+        <div className="double-bounce1"></div>
+        <div className="double-bounce2"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Preloader;
