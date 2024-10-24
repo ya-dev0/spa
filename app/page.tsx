04d117206f6a5697ae2166d9f1ebc04edcb74beb
ya-dev0 <demoyuam1@proton.me>
2024-10-24 14:18:27 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-import { fetchMovies } from "./action";
-import MovieList from "../components/MovieList";
+import { fetchRecipes } from "./action";
+import RecipeList from "../components/RecipeList";
 
 async function Home() {
-  const data = await fetchMovies();
+  const data = await fetchRecipes();
 
-  
+
+  const recipes = data.map((item) => ({
+    id: item.id,
+    recipe: item.recipe,
+    image: item.image,
+    difficulty: item.difficulty,
+    calories: item.calories,
+  }));
+
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
-      <h2 className="text-3xl text-white font-bold">Explore Movies</h2>
+      <h2 className="text-3xl text-white font-bold">Explore Recipes</h2>
 
-      <MovieList initialData={data} />
+      <RecipeList initialData={recipes} />
     </main>
   );
 }
