aa2cfb0132c3a8e9636b544b8dff73bf8588e89e
ya-dev0 <demoyuam1@proton.me>
2024-10-24 14:59:06 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+// components/Footer.tsx
+
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,38 +16,32 @@ function Footer() {
       />
       <p className="text-base font-bold text-white">@2023 KetoRecipeHub</p>
       <div className="flex items-center gap-6">
-        <Link href="https://www.facebook.com" passHref>
-          <a target="_blank" rel="noopener noreferrer">
-            <Image
-              src="/facebook.svg"
-              alt="Facebook"
-              width={19}
-              height={19}
-              className="object-contain"
-            />
-          </a>
+        <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/facebook.svg"
+            alt="Facebook"
+            width={19}
+            height={19}
+            className="object-contain cursor-pointer"
+          />
         </Link>
-        <Link href="https://www.instagram.com" passHref>
-          <a target="_blank" rel="noopener noreferrer">
-            <Image
-              src="/instagram.svg"
-              alt="Instagram"
-              width={19}
-              height={19}
-              className="object-contain"
-            />
-          </a>
+        <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/instagram.svg"
+            alt="Instagram"
+            width={19}
+            height={19}
+            className="object-contain cursor-pointer"
+          />
         </Link>
-        <Link href="https://www.twitter.com" passHref>
-          <a target="_blank" rel="noopener noreferrer">
-            <Image
-              src="/twitter.svg"
-              alt="Twitter"
-              width={19}
-              height={19}
-              className="object-contain"
-            />
-          </a>
+        <Link href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+          <Image
+            src="/twitter.svg"
+            alt="Twitter"
+            width={19}
+            height={19}
+            className="object-contain cursor-pointer"
+          />
         </Link>
       </div>
     </footer>
