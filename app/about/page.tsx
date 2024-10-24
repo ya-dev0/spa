81580463977059138d41a672000e57d63a24fe42
ya-dev0 <demoyuam1@proton.me>
2024-10-24 15:00:00 +0300

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+
+
+import React from "react";
+
+function About() {
+  return (
+    <main className="sm:p-16 py-16 px-8 flex flex-col items-center gap-6">
+      <h1 className="text-4xl text-white font-bold">About Us</h1>
+      <p className="text-lg text-gray-300 max-w-3xl text-center">
+        At KetoRecipeHub, we are passionate about providing you with the best keto recipes to support your healthy lifestyle. Our team is dedicated to curating a diverse selection of delicious and easy-to-follow recipes that make keto dieting enjoyable and sustainable.
+      </p>
+    </main>
+  );
+}
+
+export default About;
